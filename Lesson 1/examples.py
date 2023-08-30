c1d62618d12053b56226b150e1dbeb577a7d42e1
burprop <danilburzua@gmail.com>
2023-08-30 21:07:53 +0300

    Examples for first lesson

---
+++ Lesson 1/examples.py
@@ -0,0 +1,40 @@
+# Вывести текст на экран
+print("Hello, world")
+print(12345)
+
+# Объявить переменную
+variable = "Hello"
+var = 5
+another_var = 1.23
+yet_another_var = 3/12 + 6
+1number = 5 # Ошибка - нельзя начинать имя переменной с цифры
+
+# Получить данные от пользователя
+input()
+
+# Получить данные и записать в переменную
+more_var = input()
+
+# Конкатенация строк
+"Hello " + "world" == "Hello world"
+
+# Конкатенация + переменные
+name = "Ivan"
+"Hello " + name == "Hello Ivan"
+
+# Разница между строками и числами
+2 + 5 == 7
+"2" + "5" == "25"
+
+string = input()  # Строка
+number = int(input())  # Число
+
+int("2") == 2  # Как получить число из строки
+str(2) == "2"  # Как получить строку из числа
+
+# Сумма двух чисел
+number1 = int(input())
+number2 = int(input())
+print(str(number1) + " + " + str(number2) + " = " + str(number1 + number2))
+
+
